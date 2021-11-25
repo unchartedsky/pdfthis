741f906ad1bb94ff9fdb721604abb41675dfccec
Jaehoon Choi <plaintext@andromedarabbit.net>
2021-11-25 21:48:39 +0900

    깨진 링크 처리

--- tests/test_gdown.py
+++ tests/test_gdown.py
@@ -5,14 +5,8 @@ import pytest
 
 
 class GoogleDriveTests(unittest.TestCase):
-    def test_gdown(self):
-        url = 'https://drive.google.com/uc?id=0B9P1L--7Wd2vNm9zMTJWOGxobkU'
-        output = '/tmp/20150428_collected_images.tgz'
-        r = gdown.download(url, output, quiet=False)
-        self.assertEqual(r, output)
-
     @pytest.mark.skip(reason="in progress")
-    def test_gdown_2(self):
+    def test_gdown(self):
         url = 'https://drive.google.com/file/d/1XXVsAD-3uYoYMU-LJZ9AllzREQgNuvIY'
         output = '/tmp/20150428_collected_images.pdf'
         r = gdown.download(url, output, quiet=False)
